6d5684256549e39a02081e80e075fe63488bb3a5
bawmee <ppr8769@likelion.org>
2019-10-11 00:15:26 +0900

    saleFuction

--- changeSale.py
+++ changeSale.py
@@ -2,9 +2,11 @@ import json
 import sys
 import logging
 import pymysql
+import ast 
 
 
 host=
+username=
 pw=
 db=
 
@@ -26,13 +28,19 @@ def lambda_handler(event, context):
     
     conn = pymysql.connect(host=host, user=username, password=pw, db=db,charset='utf8')
     cursor = conn.cursor()
-    rst = 0
+    
     result = dict()
-
-    if event['isSale'] is 1: #세일한다고 바꿀 때 
-        sale = Sale(event['product_id'], event['saleType'], event['percent'], event['eventType'],event['startdate'], event['enddate'])
-        query1 = "UPDATE product SET isSale = 1 WHERE product_id= " + sale.product_id
-        query2 = "insert into sale(sale_type,sale_percent,sale_startdate,sale_enddate,product_id,sale_eventtype) values(" + sale.saleType + ", " + sale.percent + ", \'" + sale.startdate + "\', \'" + sale.enddate + "\', " + sale.product_id + ", \'" + sale.eventType +"\')"
+    result['isSuccess'] = 0
+    
+    data = event.get('body')
+    jdata = json.loads(data)
+    jsondata = json.loads(jdata)
+    
+    if jsondata.get('isSale') is 1: #세일한다고 바꿀 때 
+        sale = Sale(jsondata.get('product_id'), jsondata.get('saleType'), jsondata.get('percent'), jsondata.get('eventType'),jsondata.get('startdate'), jsondata.get('enddate'))
+        
+        query1 = "UPDATE product SET isSale = 1 WHERE product_id= " + str(sale.product_id)
+        query2 = "insert into sale(sale_type,sale_percent,sale_startdate,sale_enddate,product_id,sale_eventtype) values(" + str(sale.saleType) + ", " + str(sale.percent) + ", \'" + sale.startdate + "\', \'" + sale.enddate + "\', " + str(sale.product_id) + ", \'" + sale.eventType +"\')"
 
         rst1 = cursor.execute(query1)
         rst2 = cursor.execute(query2)
@@ -42,12 +50,11 @@ def lambda_handler(event, context):
             result['isSuccess'] = 1 
         else:
             result['isSuccess'] = 0
-            
-    
-    if event['isSale'] is 0: #세일안한다고 바꿀 때 
-        query2 = "UPDATE product SET isSale = 0 WHERE product_id= " + event['product_id']
-        rst = cursor.execute(query2)
         
+    if jsondata.get('isSale') is 0: #세일안한다고 바꿀 때 
+        query2 = "UPDATE product SET isSale = 0 WHERE product_id = " + str(jsondata.get('product_id'))
+        rst = cursor.execute(query2)
+            
         if rst is 1:
             conn.commit()
             result['isSuccess'] = 1 
@@ -55,10 +62,11 @@ def lambda_handler(event, context):
         else:
             result['isSuccess'] = 0
     
+    
     cursor.close()
     conn.close()
-    
-    
+        
+        
     return {
         'statusCode': 200,
         'body': json.dumps(result)
