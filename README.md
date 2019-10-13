ab58b5e5e6a17aea7ac978aa0cb539a62e8c3589
bawmee <ppr8769@likelion.org>
2019-10-13 15:44:49 +0900

    상품등록수정

--- README.md
+++ README.md
@@ -1,8 +1,101 @@
 ## allproduct.py   
   
-  필요 request 없음  
-  
-  
+  1. READ (모든 상품 정보 읽기)  
+    + 필요 request 없음
+
+  2. CREAT (상품 정보 등록)
+    + httpmethod : **POST**  
+      
+    +  request body
+        - common 
+        **변수 8개 모두 타입 무관**    
+           
+        >① "product_type" : *common(고정값 - 테이블 이름임)*       
+        >② "common_brand" : *상품 브랜드*       
+        >③ "common_category" : *상품 종류*    
+        >④ "common_name" : *이름*      
+        >⑤ "common_price" : *가격*    
+        >⑥ "common_capacity" : *용량*    
+        >⑦ "common_unit" : *용량 단위* 
+        >⑧ "common_displayType" : *큰디스플레이인지 작은 디스플레이 인지*
+
+
+        -  beef
+         **변수 7개 모두 타입 무관**    
+           
+        >① "product_type" : *beef(고정값 - 테이블 이름임)*       
+        >② "beef_part" : *부위*       
+        >③ "beef_pricePerUnitWeight" : *단위별가격*    
+        >④ "beef_origin" : *원산지*      
+        >⑤ "beef_usage" : *용도*    
+        >⑥ "beef_identificationNum" : *식별번호*    
+        >⑦ "beef_rate" : *등급* 
+           
+      
+        -  wine
+         **변수 8개 모두 타입 무관**    
+           
+        >① "product_type" : *wine(고정값 - 테이블 이름임)*       
+        >② "wine_brand" : *브랜드*       
+        >③ "wine_name" : *이름*    
+        >④ "wine_price" : *가격*      
+        >⑤ "wine_capacity" : *용량*    
+        >⑥ "wine_origin" : *원산지*    
+        >⑦ "wine_sugarContent" : *당도* 
+        >⑧ "wine_frequency" : *도수*
+
+    +  response body
+        >{isSuccess :1 or 0}
+
+  3. UPDATE (상품 정보 수정)
+    + httpmethod : **PUT**  
+      
+    +  request body
+      * create 에서 변수 ( product_id ) 1개 씩 추가됨
+        - common 
+        **변수 9개 모두 타입 무관**    
+        
+        >ⓞ "product_id" : *상품 id(기본키)*
+        >① "product_type" : *common(고정값 - 테이블 이름임)*       
+        >② "common_brand" : *상품 브랜드*       
+        >③ "common_category" : *상품 종류*    
+        >④ "common_name" : *이름*      
+        >⑤ "common_price" : *가격*    
+        >⑥ "common_capacity" : *용량*    
+        >⑦ "common_unit" : *용량 단위* 
+        >⑧ "common_displayType" : *큰디스플레이인지 작은 디스플레이 인지*
+
+
+        -  beef
+         **변수 8개 모두 타입 무관**    
+
+        >ⓞ "product_id" : *상품 id(기본키)*
+        >① "product_type" : *beef(고정값 - 테이블 이름임)*       
+        >② "beef_part" : *부위*       
+        >③ "beef_pricePerUnitWeight" : *단위별가격*    
+        >④ "beef_origin" : *원산지*      
+        >⑤ "beef_usage" : *용도*    
+        >⑥ "beef_identificationNum" : *식별번호*    
+        >⑦ "beef_rate" : *등급* 
+           
+      
+        -  wine
+         **변수 8개 모두 타입 무관**    
+           
+        >ⓞ "product_id" : *상품 id(기본키)*
+        >① "product_type" : *wine(고정값 - 테이블 이름임)*       
+        >② "wine_brand" : *브랜드*       
+        >③ "wine_name" : *이름*    
+        >④ "wine_price" : *가격*      
+        >⑤ "wine_capacity" : *용량*    
+        >⑥ "wine_origin" : *원산지*    
+        >⑦ "wine_sugarContent" : *당도* 
+        >⑧ "wine_frequency" : *도수*
+
+    +  response body
+        >{isSuccess :1 or 0}
+
+      
   
   
 ## changeSale.py  
@@ -18,7 +111,7 @@
         >③ "saleType" : *0:정기세일  1:타임세일*    
         >④ "percent" : *할인율*      
         >⑤ "eventType" : *percent B1G1 B1G2*    
-       > ⑥ "startdate" : *format( 2019-09-15 03:46:10 )*    
+        > ⑥ "startdate" : *format( 2019-09-15 03:46:10 )*    
         >⑦"enddate" : *format( 2019-09-15 03:46:10 )* `    
             
     
@@ -33,6 +126,14 @@
   
   
   
+  
+
+
+
+
+
+
+
   
 ## changeCost.py
 
--- allproduct.py
+++ allproduct.py
@@ -2,6 +2,7 @@ import sys
 import logging
 import pymysql
 import json
+from urllib.parse import urlsplit, parse_qsl
 
 
 host=
@@ -11,131 +12,228 @@ db=
 
 
 def lambda_handler(event, context):
-
+    
     #db 연결
     conn = pymysql.connect(host=host, user=username, password=pw, db=db,charset='utf8')
     curs = conn.cursor()
-    
-    
     result = dict()
     
     
-   #common 
-    query = "SELECT product.product_id,isSale,common_brand,common_name,common_price,common_capacity,common_unit,common_displayType,common_category FROM product JOIN common ON product.product_id = common.product_id;"
-    key = "common"
-    curs.execute(query)
+    httpmethod = event.get('httpMethod')
+
+    if httpmethod == "GET":
+        
+        query = "SELECT product.product_id,isSale,common_brand,common_name,common_price,common_capacity,common_unit,common_displayType,common_category FROM product JOIN common ON product.product_id = common.product_id;"
+        key = "common"
+        curs.execute(query)
     
-    result[key]=[]
+        result[key]=[]
 
-    common = curs.fetchall()
-    for col in common:
-        tmp = dict()
-        tmp['product_id'] = col[0]
-        tmp['isSale'] = col[1]
-        tmp['common_brand'] = col[2]
-        tmp['common_name'] = col[3]
-        tmp['common_price'] = col[4]
-        tmp['common_capacity'] = col[5]
-        tmp['common_unit'] = col[6]
-        tmp['common_displayType'] = col[7]
-        tmp['common_category'] = col[8]
+        common = curs.fetchall()
+        for col in common:
+            tmp = dict()
+            tmp['product_id'] = col[0]
+            tmp['isSale'] = col[1]
+            tmp['common_brand'] = col[2]
+            tmp['common_name'] = col[3]
+            tmp['common_price'] = col[4]
+            tmp['common_capacity'] = col[5]
+            tmp['common_unit'] = col[6]
+            tmp['common_displayType'] = col[7]
+            tmp['common_category'] = col[8]
 
-        if  tmp['isSale'] is 1:
-            query = "select sale_type,sale_eventtype,sale_percent,sale_startdate,sale_enddate FROM sale WHERE product_id="+ str(tmp['product_id']) + " ORDER BY sale_id DESC limit 1"
-            curs.execute(query)
-            row2 = curs.fetchone()
-            t = dict()
-            t['sale_type'] = row2[0]
-            t['sale_eventtype'] = row2[1]
-            t['sale_percent'] = row2[2]
-            t['sale_startdate'] = str(row2[3])
-            t['sale_enddate'] = str(row2[4])
-            tmp['sale_info'] = t
+            if  tmp['isSale'] is 1:
+                query = "select sale_type,sale_eventtype,sale_percent,sale_startdate,sale_enddate FROM sale WHERE product_id="+ str(tmp['product_id']) + " ORDER BY sale_id DESC limit 1"
+                curs.execute(query)
+                row2 = curs.fetchone()
+                t = dict()
+                t['sale_type'] = row2[0]
+                t['sale_eventtype'] = row2[1]
+                t['sale_percent'] = row2[2]
+                t['sale_startdate'] = str(row2[3])
+                t['sale_enddate'] = str(row2[4])
+                tmp['sale_info'] = t
             
-        result[key].append(tmp) 
-
-
-    
-
-
-
-
-    #beef 
-    query = "SELECT product.product_id,isSale,beef_part,beef_pricePerUnitWeight,beef_origin,beef_usage,beef_identificationNum,beef_rate FROM product,beef WHERE product.product_id = beef.product_id"
-    key = "beef"
-    curs.execute(query)
-
-    result[key]=[]
-
-
-
-    beef = curs.fetchall()
-    for col in beef:
-        tmp = dict()
-        tmp['product_id'] = col[0]
-        tmp['isSale'] = col[1]
-        tmp['beef_part'] = col[2]
-        tmp['beef_pricePerUnitWeight'] = col[3]
-        tmp['beef_origin'] = col[4]
-        tmp['beef_usage'] = col[5]
-        tmp['beef_identificationNum'] = col[6]
-        tmp['beef_rate'] = col[7]
-
-        if  tmp['isSale'] is 1:
-            query = "select sale_type,sale_eventtype,sale_percent,sale_startdate,sale_enddate FROM sale WHERE product_id="+ str(tmp['product_id']) + " ORDER BY sale_id DESC limit 1"
-            curs.execute(query)
-            row2 = curs.fetchone()
-            t = dict()
-            t['sale_type'] = row2[0]
-            t['sale_eventtype'] = row2[1]
-            t['sale_percent'] = row2[2]
-            t['sale_startdate'] = str(row2[3])
-            t['sale_enddate'] = str(row2[4])
-            tmp['sale_info'] = t
+            result[key].append(tmp)
             
-        result[key].append(tmp) 
+        
+        
+        #beef 
+        query = "SELECT product.product_id,isSale,beef_part,beef_pricePerUnitWeight,beef_origin,beef_usage,beef_identificationNum,beef_rate FROM product,beef WHERE product.product_id = beef.product_id"
+        key = "beef"
+        curs.execute(query)
 
-     
+        result[key]=[]
+
+
+
+        beef = curs.fetchall()
+        for col in beef:
+            tmp = dict()
+            tmp['product_id'] = col[0]
+            tmp['isSale'] = col[1]
+            tmp['beef_part'] = col[2]
+            tmp['beef_pricePerUnitWeight'] = col[3]
+            tmp['beef_origin'] = col[4]
+            tmp['beef_usage'] = col[5]
+            tmp['beef_identificationNum'] = col[6]
+            tmp['beef_rate'] = col[7]
+
+            if  tmp['isSale'] is 1:
+                query = "select sale_type,sale_eventtype,sale_percent,sale_startdate,sale_enddate FROM sale WHERE product_id="+ str(tmp['product_id']) + " ORDER BY sale_id DESC limit 1"
+                curs.execute(query)
+                row2 = curs.fetchone()
+                t = dict()
+                t['sale_type'] = row2[0]
+                t['sale_eventtype'] = row2[1]
+                t['sale_percent'] = row2[2]
+                t['sale_startdate'] = str(row2[3])
+                t['sale_enddate'] = str(row2[4])
+                tmp['sale_info'] = t
+            
+            result[key].append(tmp) 
+   
+        
+        #wine
+        query = "SELECT product.product_id,isSale,wine_brand,wine_name,wine_price,wine_capacity,wine_origin,wine_sugarContent,wine_frequency FROM product,wine WHERE product.product_id = wine.product_id;"
+        key = "wine"
+        curs.execute(query)
+
+        result[key]=[]
+
+        wine = curs.fetchall()
+        for col in wine:
+            tmp = dict()
+            tmp['product_id'] = col[0]
+            tmp['isSale'] = col[1]
+            tmp['wine_brand'] = col[2]
+            tmp['wine_name'] = col[3]
+            tmp['wine_price'] = col[4]
+            tmp['wine_capacity'] = col[5]
+            tmp['wine_origin'] = col[6]
+            tmp['wine_sugarContent'] = col[7]
+            tmp['wine_frequency'] = col[8]
+
+            if  tmp['isSale'] is 1:
+                query = "select sale_type,sale_eventtype,sale_percent,sale_startdate,sale_enddate FROM sale WHERE product_id="+ str(tmp['product_id']) + " ORDER BY sale_id DESC limit 1"
+                curs.execute(query)
+                row2 = curs.fetchone()
+                t = dict()
+                t['sale_type'] = row2[0]
+                t['sale_eventtype'] = row2[1]
+                t['sale_percent'] = row2[2]
+                t['sale_startdate'] = str(row2[3])
+                t['sale_enddate'] = str(row2[4])
+                tmp['sale_info'] = t
+            
+            result[key].append(tmp)    
         
     
+    elif httpmethod == "POST":
+        data = event.get('body')
+        param = dict(parse_qsl(urlsplit(data).path))
+        product_type = param.get('product_type')
+
         
-    #wine
-    query = "SELECT product.product_id,isSale,wine_brand,wine_name,wine_price,wine_capacity,wine_origin,wine_sugarContent,wine_frequency FROM product,wine WHERE product.product_id = wine.product_id;"
-    key = "wine"
-    curs.execute(query)
-
-    result[key]=[]
-
-    wine = curs.fetchall()
-    for col in wine:
-        tmp = dict()
-        tmp['product_id'] = col[0]
-        tmp['isSale'] = col[1]
-        tmp['wine_brand'] = col[2]
-        tmp['wine_name'] = col[3]
-        tmp['wine_price'] = col[4]
-        tmp['wine_capacity'] = col[5]
-        tmp['wine_origin'] = col[6]
-        tmp['wine_sugarContent'] = col[7]
-        tmp['wine_frequency'] = col[8]
-
-        if  tmp['isSale'] is 1:
-            query = "select sale_type,sale_eventtype,sale_percent,sale_startdate,sale_enddate FROM sale WHERE product_id="+ str(tmp['product_id']) + " ORDER BY sale_id DESC limit 1"
-            curs.execute(query)
-            row2 = curs.fetchone()
-            t = dict()
-            t['sale_type'] = row2[0]
-            t['sale_eventtype'] = row2[1]
-            t['sale_percent'] = row2[2]
-            t['sale_startdate'] = str(row2[3])
-            t['sale_enddate'] = str(row2[4])
-            tmp['sale_info'] = t
+        rst1 = 0
+        query1 = "insert into product(isSale, product_type) values(0, \'" + product_type + "\')"
+        rst1 = curs.execute(query1)
+        if product_type == 'common':
+            query2 = "insert into common(product_id, common_brand, common_name, common_price, common_capacity, common_unit, common_displayType, common_category) "
+            query2 += "values(LAST_INSERT_ID(), " 
+            query2 += "\'" + param['common_brand'] + "\', "
+            query2 += "\'" + param['common_name'] + "\', " 
+            query2 += param['common_price'] + ", " 
+            query2 += param['common_capacity'] + ", " 
+            query2 += "\'" + param['common_unit'] + "\', " 
+            query2 += "\'" + param['common_displayType'] + "\', " 
+            query2 += "\'" + param['common_category'] + "\')"    
+        
+        elif product_type == 'beef':
+            query2 = "insert into beef(product_id, beef_part, beef_pricePerUnitWeight, beef_origin, beef_usage, beef_identificationNum, beef_rate) "
+            query2 += "values(LAST_INSERT_ID(), " 
+            query2 += "\'" + param['beef_part'] + "\', " 
+            query2 += param['beef_pricePerUnitWeight'] + ", " 
+            query2 += "\'" + param['beef_origin'] + "\', " 
+            query2 += "\'" + param['beef_usage'] + "\', " 
+            query2 += param['beef_identificationNum'] + ", " 
+            query2 += "\'" + param['beef_rate'] + "\')"
             
-        result[key].append(tmp)
+        elif product_type == 'wine':
+            query2 = "insert into wine(product_id, wine_brand, wine_name, wine_price, wine_capacity, wine_origin, wine_sugarContent, wine_frequency) "
+            query2 += "values(LAST_INSERT_ID(), " 
+            query2 += "\'" + param['wine_brand'] + "\', " 
+            query2 += "\'" + param['wine_name'] + "\', " 
+            query2 += param['wine_price'] + ", " 
+            query2 += param['wine_capacity'] + ", " 
+            query2 += "\'" + param['wine_origin'] + "\', " 
+            query2 += param['wine_sugarContent'] + ", " 
+            query2 += param['wine_frequency'] + ")"    
+        rst2 = 0
+        rst2 = curs.execute(query2)
 
+        if rst1 == 1 and rst2 == 1:
+            result['isSuccess'] = 1
+            conn.commit()
+                
+        else:
+            result['isSuccess'] = 0
+      
+            
+    elif httpmethod == "PUT":
+        data = event.get('body')
+        param = dict(parse_qsl(urlsplit(data).path))
+        product_type = param.get('product_type')
+        
+        
+        if product_type == 'common':
+            query = "UPDATE common SET "
+            query += "common_brand = \'" + param['common_brand'] + "\', "
+            query += "common_name = \'" + param['common_name'] + "\', " 
+            query += "common_price = " + param['common_price'] + ", " 
+            query += "common_capacity = " + param['common_capacity'] + ", " 
+            query += "common_unit = \'" + param['common_unit'] + "\', " 
+            query += "common_displayType = \'" + param['common_displayType'] + "\', " 
+            query += "common_category = \'" + param['common_category'] + "\' "    
+            query += "where product_id=" + param['product_id']
+        
+        
+        elif product_type == 'beef':
+            query = "UPDATE beef SET "
+            query += "beef_part = \'" + param['beef_part'] + "\', "  
+            query += "beef_pricePerUnitWeight = " + param['beef_pricePerUnitWeight'] + ", " 
+            query += "beef_origin = \'" + param['beef_origin'] + "\', " 
+            query += "beef_usage= \'" + param['beef_usage'] + "\', " 
+            query += "beef_identificationNum = " + param['beef_identificationNum'] + ", " 
+            query += "beef_rate = \'" + param['beef_rate'] + "\' "
+            query += "where product_id=" + param['product_id']
+            
+            
+        elif product_type == 'wine':
+            query = "UPDATE wine SET "
+            query += "wine_brand = \'" + param['wine_brand'] + "\', " 
+            query += "wine_name = \'" + param['wine_name'] + "\', " 
+            query += "wine_price = " + param['wine_price'] + ", " 
+            query += "wine_capacity = " + param['wine_capacity'] + ", " 
+            query += "wine_origin = \'" + param['wine_origin'] + "\', " 
+            query += "wine_sugarContent = " + param['wine_sugarContent'] + ", " 
+            query += "wine_frequency  = " + param['wine_frequency'] + " "  
+            query += "where product_id=" + param['product_id']
+        
+        
+        rst = 0
+        rst = curs.execute(query)
+        
+        if rst == 1:
+            result['isSuccess'] = 1
+            conn.commit()
+        else:
+            result['isSuccess'] = 0
+            
+            
 
-    
-
+        
+        
     
     curs.close()
     conn.close()
